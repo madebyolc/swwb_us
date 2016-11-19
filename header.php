1b5a77d537e9ef24cecd72c355a4c5b5de3560ff
§ <madebyolc@gmail.com>
2016-11-19 14:50:57 +0000

    Pushmenu & minimal header

--- header.php
+++ header.php
@@ -17,55 +17,95 @@
 <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<script src="//code.jquery.com/jquery.min.js"></script>
+
 <?php wp_head(); ?>
-</head>
+</head
 
-<body <?php body_class(); ?>>
+<body <?php body_class( "pushmenu-push" ); ?>>
 
-<div id="page" class="hfeed site">
+<swwbwrap class="pushmenu-push">
 
-    <!-- ******************* The Navbar Area ******************* -->
+  <nav class="pushmenu pushmenu-left">
+
+    <div class="wrap-pushmenu container">
+
+      <nav class="navbar site-navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+
+        <div class="push-esc">
+
+          <button class="nav_close navbar-toggler visible-xs-up" type="button" data-toggle="collapse" data-target=".exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Close navigation</span>
+            <span class="icon-bar one"></span>
+            <span class="icon-bar two"></span>
+            <span class="icon-bar three"></span>
+          </button>
+
+        </div> <!-- .push-esc -->
+
+        <div class="push-block">
+
+          <form method="get" id="searchform" class="form lg lines-dark" action="//localhost:3000/swwb/" role="search" _lpchecked="1">
+        		<label for="s" class="assistive-text sr-only">Search</label>
+        		<div class="input-group">
+        			<input type="text" class="field form-control" name="s" id="s" placeholder="Search …">
+        			<span class="input-group-btn">
+        				<input type="submit" class="submit btn btn-primary" name="submit" id="searchsubmit" value="Search">
+        			</span>
+        		</div>
+        	</form>
+
+        </div> <!-- .push-block .search -->
+
+        <div class="push-block">
+
+        </div> <!-- .push-block -->
+
+      </nav> <!-- .site-navigation -->
+
+    </div>
+
+  </nav>
+
+  <div id="page" class="hfeed site">
+
     <div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
 
-        <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'understrap' ); ?></a>
+      <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'understrap' ); ?></a>
 
-        <nav class="navbar site-navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+      <nav class="navbar site-navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+
+        <div class="container">
+
+          <div class="wrap-explore">
+
+            <button class="nav_list navbar-toggler visible-xs-up" type="button" data-toggle="collapse" data-target=".exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
+      				<span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar one"></span>
+              <span class="icon-bar two"></span>
+              <span class="icon-bar three"></span>
+            </button>
+
+          </div> <!-- .wrap-explore -->
+
+          <div class="wrap-brand">
+
+            <?php if (!has_custom_logo()) { ?>
+              <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+              	<?php bloginfo( 'name' ); ?>
+              </a>
+            <?php } else { the_custom_logo(); } ?><!-- end custom logo -->
+
+          </div> <!-- .wrap-brand -->
+
+          <div class="wrap-meta">
 
 
-                <div class="container">
 
+          </div> <!-- .wrap-meta -->
 
-                            <div class="navbar-header">
+        </div> <!-- .container -->
 
-                                <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+      </nav><!-- .site-navigation -->
 
-                                <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target=".exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
-    					&#9776;
-  				</button>
-
-                                <!-- Your site title as branding in the menu -->
-	                                <?php if (!has_custom_logo()) { ?>
-		                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-		                                	<?php bloginfo( 'name' ); ?>
-		                                </a>
-	                                <?php } else { the_custom_logo(); } ?><!-- end custom logo -->
-
-                            </div>
-
-                            <!-- The WordPress Menu goes here -->
-                            <?php wp_nav_menu(
-                                    array(
-                                        'theme_location' => 'primary',
-                                        'container_class' => 'collapse navbar-toggleable-xs exCollapsingNavbar',
-                                        'menu_class' => 'nav navbar-nav',
-                                        'fallback_cb' => '',
-                                        'menu_id' => 'main-menu',
-                                        'walker' => new wp_bootstrap_navwalker()
-                                    )
-                            ); ?>
-
-                </div> <!-- .container -->
-
-        </nav><!-- .site-navigation -->
-
-    </div><!-- .wrapper-navbar end -->
+  </div><!-- .wrapper-navbar end -->
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,9 +6,10 @@
  */
 
 function understrap_scripts() {
-    wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.4.7');
-    wp_enqueue_script('jquery'); 
-    wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.4.7', true );
+    wp_enqueue_style('understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.4.7');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.4.7', true );
+    wp_enqueue_script('ux', get_template_directory_uri() . '../js/ux.js');
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
@@ -17,15 +18,15 @@ function understrap_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'understrap_scripts' );
 
-/** 
+/**
 *Loading slider script conditionally
 **/
 
 if ( is_active_sidebar( 'hero' ) ):
 add_action("wp_enqueue_scripts","understrap_slider");
-  
+
 function understrap_slider(){
-    if ( is_front_page() ) {    
+    if ( is_front_page() ) {
     $data = array(
         "timeout"=> intval( get_theme_mod( 'understrap_theme_slider_time_setting', 5000 )),
         "items"=> intval( get_theme_mod( 'understrap_theme_slider_count_setting', 1 ))
@@ -36,4 +37,3 @@ function understrap_slider(){
     }
 }
 endif;
-
